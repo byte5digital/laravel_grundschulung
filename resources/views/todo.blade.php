242c1d023732c9a2727900df2bae1a9e29760f18
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-02-09 13:21:57 +0100

    display errors - task 8

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -20,9 +20,14 @@
                     <form action="{{ route('tasks.store') }}" method="POST">
                         @csrf
 
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('subject') ? 'has-danger' : '' }}">
                             <label for="subject">Aufgaben Text</label>
-                            <input type="text" name="subject" />
+                            <input type="text" name="subject" value="{{ old('subject') }}" class="form-control" style="{{ $errors->has('subject') ? 'border-color:red;' : '' }}" />
+                            @if ($errors->has('subject'))
+                                <div class="form-control-feedback" style="color: red;">
+                                    {{  $errors->first('subject') }}
+                                </div>
+                            @endif
                         </div>
                         <div class="btn-group">
                             <button type="submit" class="btn btn-primary">Absenden</button>
