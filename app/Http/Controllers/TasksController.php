81da3025c720601fbededa2efec22c63873fcab9
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-02-09 13:10:28 +0100

    display tasks - task 7

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,7 +14,9 @@ class TasksController extends Controller
      */
     public function index()
     {
-        return view('todo');
+        $tasks = Task::all();
+
+        return view('todo', ['tasks' => $tasks]);
     }
 
     /**
--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,18 +1,36 @@
 <!doctype html>
 <html>
+<head>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+</head>
 <body>
-    <div class="container">
-        <form action="{{ route('tasks.store') }}" method="POST">
-            @csrf
+    <section class="mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Aufgaben</h2>
+                    <ul>
+                        @foreach ($tasks as $task)
+                            <li>{{ $task->subject }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h2>Erstelle eine neue Aufgabe</h2>
+                    <form action="{{ route('tasks.store') }}" method="POST">
+                        @csrf
 
-            <div class="form-group">
-                <label for="subject">Aufgaben Text</label>
-                <input type="text" name="subject" />
+                        <div class="form-group">
+                            <label for="subject">Aufgaben Text</label>
+                            <input type="text" name="subject" />
+                        </div>
+                        <div class="btn-group">
+                            <button type="submit" class="btn btn-primary">Absenden</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="btn-group">
-                <button type="submit" class="btn btn-primary">Absenden</button>
-            </div>
-        </form>
-    </div>
+        </div>
+    </section>
 </body>
 </html>
